d926b3380dbe9ac6ba6168eb7ca58a5db414bf1b
Ankur Pathak <ankurpathak@live.in>
2018-10-21 20:28:08 +0530

    First part done

--- backend/src/main/java/com/ankurpathak/myapp/MyappApplication.java
+++ backend/src/main/java/com/ankurpathak/myapp/MyappApplication.java
@@ -1,12 +1,33 @@
 package com.ankurpathak.myapp;
 
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
+@RestController
 public class MyappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyappApplication.class, args);
     }
+
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
-  greeting = {'id': 'XXX', 'content': 'Hello World'};
+  // greeting = {'id': 'XXX', 'content': 'Hello World'};
+  greeting = {};
+  constructor(private http: HttpClient) {
+    this.http.get('resource').subscribe(data => {
+      return this.greeting = data;
+    });
+  }
 }
